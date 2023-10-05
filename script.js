8d1e8039e4ae661f2849e1e5ca79ab1c9e85d1d9
alainefsc1~ <alaineluzdivina@gmail.com>
2023-10-05 00:30:37 -0300

    mudança de layout e responsividade

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+
+function ativaLetra(elemento) {
+    const arrTexto = elemento.innerHTML.split(''); 
+    elemento.innerHTML = '';
+    arrTexto.forEach((letra, index) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra; 
+        }, 75 * index);
+    });
+}
+
+const título = document.querySelector('.digitando');
+ativaLetra(título);
+
+const ativaMenu = document.querySelector('.fa-bars'); 
+ativaMenu.addEventListener('click', () => { 
+    ativaMenu.classList.toggle('fa-x'); 
+});
